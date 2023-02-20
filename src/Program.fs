7afc29869d56b94daacaca404a91cdb2634c4961
osawa-koki <github@osawa.dev>
2023-02-20 11:10:07 +0900

    HelloWorld

--- src/Program.fs
+++ src/Program.fs
@@ -1,2 +1,7 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+
+[<EntryPoint>]
+let main argv = 
+  // アプリケーションのロジックを実装する
+  printfn "Hello, world!"
+  // 終了コード0を返す
+  0
