bc98948abb688d7c993d458209cd0d9cee92346e
osawa-koki <github@osawa.dev>
2023-02-20 11:22:07 +0900

    小さな修正。

--- src/Display.fs
+++ src/Display.fs
@@ -1,5 +1,6 @@
 
 module Display
-  let Display (title: string) (arr: int []) =
-    let paddedTitle = title.PadRight(20)
-    printfn "%s: %s" paddedTitle (String.concat " " (Array.map string arr))
+
+let Display (title: string) (arr: int []) =
+  let paddedTitle = title.PadRight(20)
+  printfn "%s: %s" paddedTitle (String.concat " " (Array.map string arr))
